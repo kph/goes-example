cf08f29b0c69af284e768b6680056c1e26ac153c
Kevin Paul Herbert <kph@platinasystems.com>
2019-02-08 17:45:42 -0800

    goes: add sshd

--- goes.go
+++ goes.go
@@ -63,6 +63,7 @@ import (
 	"github.com/platinasystems/goes/cmd/slashinit"
 	"github.com/platinasystems/goes/cmd/sleep"
 	"github.com/platinasystems/goes/cmd/source"
+	"github.com/platinasystems/goes/cmd/sshd"
 	"github.com/platinasystems/goes/cmd/start"
 	"github.com/platinasystems/goes/cmd/stop"
 	"github.com/platinasystems/goes/cmd/stty"
@@ -164,6 +165,7 @@ var Goes = &goes.Goes{
 		"/init":     &slashinit.Command{},
 		"sleep":     sleep.Command{},
 		"source":    &source.Command{},
+		"sshd":      &sshd.Command{},
 		"start":     &start.Command{},
 		"stop":      &stop.Command{},
 		"stty":      stty.Command{},
