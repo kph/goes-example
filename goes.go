e940555b2bac5c7b9995bdc6f85aae694f3d78d6
Kevin Paul Herbert <kph@platinasystems.com>
2019-09-09 17:59:34 -0700

    goes-example: Add ldp command
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- goes.go
+++ goes.go
@@ -46,6 +46,7 @@ import (
 	"github.com/platinasystems/goes/cmd/kexec"
 	"github.com/platinasystems/goes/cmd/keys"
 	"github.com/platinasystems/goes/cmd/kill"
+	"github.com/platinasystems/goes/cmd/ldp"
 	"github.com/platinasystems/goes/cmd/ln"
 	"github.com/platinasystems/goes/cmd/log"
 	"github.com/platinasystems/goes/cmd/ls"
@@ -133,6 +134,7 @@ var Goes = &goes.Goes{
 		"kexec":   &kexec.Command{},
 		"keys":    keys.Command{},
 		"kill":    kill.Command{},
+		"ldp":     ldp.Command{},
 		"ln":      ln.Command{},
 		"log":     log.Command{},
 		"ls":      ls.Command{},
